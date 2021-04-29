a7da285aef61d4dbfe57f73e05d36ad1c555a089
JoacoCoyu <jacouyoumdjian@uc.cl>
2021-04-28 23:32:23 -0400

    last from today - not returning POST

--- t2_music/router.py
+++ t2_music/router.py
@@ -1,11 +1,21 @@
 from music_app.views import CarSpecViewset
 from music_app.app_views import artists, albums, artists_albums
-#from rest_framework import routers
-from rest_framework_nested import routers
+from rest_framework import routers
+from django.urls import path
+#from rest_framework_nested import routers as nested_routers
+#from music_app.views import homePageView
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 #router.register('cars', CarSpecViewset, basename='cars_info')
-router.register('artists', artists.ArtistViewset, basename='artists_info')
+router.register(r'artists', artists.ArtistViewset, basename='artists_info')
 router.register('albums', albums.AlbumViewset, basename='albums_info')
-router.register('artists/<int:artist_id>/albums',
-                artists_albums.ArtistAlbumViewset, basename='artist_album_info')
+# router.register('artists/<int:artist_id>/albums',
+#                 artists_albums.ArtistAlbumViewset, basename='artist_album_info')
+
+# urlpatterns = [
+#     path('home/', homePageView, name='index'),
+#     # path('episodio/<str:id_episodio>/', views.episodio, name='episodio'),
+#     # path('personaje/<str:nombre>/', views.personaje, name='personaje'),
+#     # path('busqueda/', views.busqueda, name='busqueda'),
+#     # path('<str:serie>/<str:id_temporada>/', views.temporada, name='temporada'),
+# ]
--- t2_music/urls.py
+++ t2_music/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .router import router
+#from .router import router
 
 urlpatterns = [
+    path('', include('music_app.urls')),
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+
 ]
