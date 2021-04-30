37b22308e0f5e9c20629bc0de88eace2ce238fea
JoacoCoyu <jacouyoumdjian@uc.cl>
2021-04-30 16:37:18 -0400

    commenting environ module

--- t2_music/settings.py
+++ t2_music/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-import environ
+#import environ
 
 # Initialise environment variables
 # env = environ.Env()
